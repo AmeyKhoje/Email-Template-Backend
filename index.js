b9c1a7382010010ea21684c49dcb69576b08078f
AmeyKhoje <ameykhoje@gmail.com>
2021-04-05 09:51:17 +0530

    first commit with basic project setup with database connection

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "email_template"
+});
+
+conn.connect((err) => {
+    if(err) throw err;
+    app.listen(port, () => console.log(`Connected to database`));
+});
